136e8b1e9ceed501727dbe935eb7c119e477fc48
luishcd <66318850+luishcd@users.noreply.github.com>
2021-02-09 20:04:27 -0200

    Inventario (Start)

---
+++ ProjetoAtivos/DAO/InventarioDAO.cs
@@ -0,0 +1,18 @@
+﻿using Survey.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAtivos.DAO
+{
+    public class InventarioDAO
+    {
+        private Banco b;
+
+        internal InventarioDAO()       //pegando instancia do objeto banco
+        {
+            b = Banco.GetInstance();
+        }
+    }
+}
---
+++ ProjetoAtivos/Models/Inventario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAtivos.Models
+{
+    public class Inventario
+    {
+        public int Codigo { get; set; }
+        public DateTime Data { get; set; }
+        public string Obs { get; set; }
+        public Ativo Ativo{ get; set; }        
+        public Filial Filial { get; set; }
+
+
+    }
+}
